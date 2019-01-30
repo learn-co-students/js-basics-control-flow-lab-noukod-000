25728269b8784882ebe4ed99f5aefaeec26e6752
Samuel Cornet <samuel@noukod.com>
2019-01-30 19:28:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(someValue) {
+function scuberGreetingForFeet(ride) {
 
     let result;
-    if (someValue <= 400) {
+    if (ride <= 400) {
        result = 'This one is on me!';
        }
-       else if (someValue > 2500) {
+       else if (ride > 2500) {
          result = 'No can do.';
        }
-       else if (someValue > 2000) {
+       else if (ride > 2000) {
          result = 'I will gladly take your thirty bucks.';
        }
 
