de0502d8a78dd94a694b7baf8bacaff978d07888
Merline Belizaire <merline@noukod.com>
2019-02-22 18:41:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 // Write your code in this file!
 function scuberGreetingForFeet (feet) {
 
-  let result;
 
   if (feet <= 400) {
-    result 'This one is on me!';
+  return 'This one is on me!';
   } else if (1999 < feet && feet < 2500) {
-    result "I will gladly take your thirty bucks.";
+    return "I will gladly take your thirty bucks.";
   } else {
-    result "No can do.";
+    return "No can do.";
   }
 }
 
 function ternaryCheckCity (city) {
-  result city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip (tip) {
   switch (tip) {
     case 'generous':
-      result 'Thank you so much.';
+      return 'Thank you so much.';
     case 'not as generous':
-      result 'Thank you.';
+      return 'Thank you.';
     default:
-      result 'Bye.';
+      return 'Bye.';
   }
-  return result;
+
 }
