ab892b4e8efe87ec030401053e0c2311f7d4128b
Certil Remy <certilremy@users.noreply.github.com>
2019-10-21 15:15:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-function scuberGreetingForFeet(distance)
-{
-
-  if (distance <= 400) {
-    return "This one is on me!"
-  }
-  else if (distance > 2000 &&  distance < 2500) {
-    return 'I will gladly take your thirty bucks.'
-  }
-  else if (distance>2500) {
-    return 'No can do.'
-  }
-}
-
-
-function ternaryCheckCity(city)
-{
-return (city =='NYC' ? 'Ok, sounds good.' : 'No go.');
-}
-
-
-function switchOnCharmFromTip(tip)
-{
-
-switch (tip) {
-  case 'generous':
- return 'Thank you so much.'
-    break;
-    case 'not as generous':
-   return 'Thank you.'
-      break;
-  default:
-  return 'Bye.'
+// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  //  ride = 400
+    if (ride <= 400 ) {
+        return "This one is on me!";
+    }
+    
+    if (ride > 2000 && ride < 2500) {
+        return "I will gladly take your thirty bucks.";
+    }
+    
+    else if(ride > 2500)
+    {
+        return 'No can do.'
+     }
 
 }
 
+function ternaryCheckCity(city) {
+      return city == 'NYC' ? 'Ok, sounds good.': 'No go.'
+}
 
+function switchOnCharmFromTip(tips) {
+    switch (tips) {
+        case 'generous':
+        return 'Thank you so much.'
+            
+            break;
+        case 'not as generous':
+        return 'Thank you.'
+            
+            break;
+    
+        default:
+        return 'Bye.'
+            break;
+    }
 }
